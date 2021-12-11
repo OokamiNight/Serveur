85b5c80f95e4a0df655c361fc7ee9c1f6b2b1876
OokamiNight <75337836+OokamiNight@users.noreply.github.com>
2021-12-10 22:18:04 -0500

    Add files via upload

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,4 +1,6 @@
-const Response = require('../response.js');
+const Response = require('../response');
+const utilities = require('../utilities');
+const TokenManager = require('../tokenManager');
 /////////////////////////////////////////////////////////////////////
 // Important note about controllers:
 // You must respect pluralize convention: 
@@ -8,13 +10,42 @@ const Response = require('../response.js');
 /////////////////////////////////////////////////////////////////////
 module.exports =
     class Controller {
-        constructor(req, res, params) {
+        constructor(req, res, params, needAuthorization = false) {
             if (req != null && res != null) {
                 this.req = req;
                 this.res = res;
                 this.response = new Response(res, this.req.url);
                 this.params = params;
+                // if true, will require a valid bearer token from request header
+                this.needAuthorization = needAuthorization;
             }
+        }
+        requestAuthorized() {
+            if (this.needAuthorization) {
+                return TokenManager.requestAuthorized(this.req);
+            }
+            return true;
+        }
+        requestActionAuthorized() {
+            return TokenManager.requestAuthorized(this.req);
+        }
+        queryStringParamsList() {
+            let content = "<div style=font-family:arial>";
+            content += "<h4>List of parameters in query strings:</h4>";
+            content += "<h4>? sort=key <br> return all words sorted by key values (word)";
+            content += "<h4>? sort=key,desc <br> return all words sorted by descending key values";
+            content += "<h4>? key=value <br> return the word with key value = value";
+            content += "<h4>? key=value* <br> return the word with key value that start with value";
+            content += "<h4>? key=*value* <br> return the word with key value that contains value";
+            content += "<h4>? key=*value <br> return the word with key value end with value";
+            content += "<h4>page?limit=int&offset=int <br> return limit words of page offset";
+            content += "</div>";
+            return content;
+        }
+        queryStringHelp() {
+            // expose all the possible query strings
+            this.res.writeHead(200, { 'content-type': 'text/html' });
+            this.res.end(this.queryStringParamsList());
         }
         paramsError(params, message) {
             if (params) {
@@ -25,7 +56,7 @@ module.exports =
             }
             return false;
         }
-        head(){
+        head() {
             this.response.notImplemented();
         }
         getAll() {
@@ -38,9 +69,6 @@ module.exports =
             this.response.notImplemented();
         }
         put(obj) {
-            this.response.notImplemented();
-        }
-        patch(obj) {
             this.response.notImplemented();
         }
         remove(id) {
